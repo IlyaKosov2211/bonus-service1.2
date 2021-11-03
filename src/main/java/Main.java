19c553e927201b6908a9058f4759b58a5e5ece7c
IlyaKosov2211 <ilakosov8@Gmail.com>
2021-11-03 19:41:27 +0700

    fourth commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
-    long amount = 1000_60;
+    long amount = 1000_50;
     boolean registered = true;
 
     long bonus= service.calculate(amount, registered);
